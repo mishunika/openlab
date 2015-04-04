032b9d2719bcd9b5530ef19bc295bfec6aadccb5
Mihai Iachimovschi <mihai.iachimovschi@gmail.com>
2015-04-04 23:36:40 +0200

    Creating a simple demo test for the proof of concept.

--- tester/evaluator.py
+++ tester/evaluator.py
@@ -1,3 +1,7 @@
+from functools import reduce
+from operator import and_ as and_lambda
+
+
 class Evaluator:
     def __init__(self):
         self.coefficients = []
@@ -14,6 +18,26 @@ class Evaluator:
         for coefficient in self.coefficients:
             score += coefficient['value'] * 100
         return score
+
+    def execute_method(self, method_name, arguments={}):
+        try:
+            method_ref = getattr(self.solution, method_name)
+            result = method_ref(**arguments)
+        except AttributeError as e:
+            print(e)
+            return None
+
+        return result
+
+    def method_test(self, method, tests, reward):
+        results = []
+        for test in tests:
+            result = self.execute_method(method, test['arguments'])
+            results.append(result == test['expected_result'])
+        passed = reduce(and_lambda, results)
+        coefficient = reward['coefficient'] if passed else 0
+        self.add_coefficient(reward['message'], coefficient)
+
 
 if __name__ == "__main__":
     e = Evaluator()
---
+++ tester/evaluator_wrapper.py
@@ -0,0 +1,1 @@
+from evaluator_test import EvaluatorTest
