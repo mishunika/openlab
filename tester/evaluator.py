9fde616cae1628d7c9db3f8ae28c64545ba8cd1c
Mihai Iachimovschi <mihai.iachimovschi@gmail.com>
2015-04-04 23:35:27 +0200

    Getting rid of mandatory test. It will be the professor's task to decide what is mandatory and what is the weight of it.

--- tester/evaluator.py
+++ tester/evaluator.py
@@ -1,21 +1,18 @@
 class Evaluator:
     def __init__(self):
-        self.mandatory_passed = False
         self.coefficients = []
 
     def add_coefficient(self, name, value):
         coefficient = {'name': str(name), 'value': round(value, 2)}
         self.coefficients.append(coefficient)
 
-    def pass_mandatory_test(self):
-        self.mandatory_passed = True
-
     def get_final_score(self):
-        if not self.mandatory_passed:
+        if self.coefficients[0]['value'] == 0:
             return float(0)
-        score = 50
+
+        score = 0
         for coefficient in self.coefficients:
-            score += coefficient['value'] * 50
+            score += coefficient['value'] * 100
         return score
 
 if __name__ == "__main__":
@@ -23,7 +20,6 @@ if __name__ == "__main__":
     e.add_coefficient('Test 1', 0.3)
     e.add_coefficient('Test 2', 0)
     e.add_coefficient('Test 3', 0.3)
-    e.pass_mandatory_test()
 
     print(e)
     print(e.coefficients)
