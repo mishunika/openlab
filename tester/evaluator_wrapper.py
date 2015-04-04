bacb1e51ccd7b76e54ba43b0093a43a39c82f046
Mihai Iachimovschi <mihai.iachimovschi@gmail.com>
2015-04-04 23:57:55 +0200

    Setting up the wrapper.

--- tester/evaluator_wrapper.py
+++ tester/evaluator_wrapper.py
@@ -1,1 +1,14 @@
+import os
+import sys
+
+stdout = sys.stdout                 # Backup STDOUT
+dev_null = open(os.devnull, 'w')    # /dev/null
+sys.stdout = dev_null               # Setting stdout to /dev/null
+
 from evaluator_test import EvaluatorTest
+
+et = EvaluatorTest()
+et.run_all_tests()
+
+sys.stdout = stdout                 # Restoring the right stdout
+print(et.get_test_results())
