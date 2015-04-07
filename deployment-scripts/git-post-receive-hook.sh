7703d053639092ea26a46711bbd1d1bbab158f98
Mihai Iachimovschi <mihai.iachimovschi@gmail.com>
2015-04-07 17:22:39 +0200

    Push to deploy formula (post-receive hook) added.

---
+++ deployment-scripts/git-post-receive-hook.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+PROJECT_ROOT="$HOME/openlab"
+
+while read oldrev newrev refname
+do
+    branch=$(git rev-parse --symbolic --abbrev-ref $refname)
+    if [ "master" == "$branch" ]; then
+        GIT_WORK_TREE="$PROJECT_ROOT" git checkout -f master
+        cd "$PROJECT_ROOT/deployment-scripts"
+        docker-compose -p openlab kill web
+        docker-compose -p openlab build web
+        docker-compose -p openlab up -d --no-recreate
+    fi
+done
