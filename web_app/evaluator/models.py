991bce6b1b7dc6d46cccfe79e783f79b95a5922f
Mihai Iachimovschi <mihai.iachimovschi@gmail.com>
2015-04-15 17:08:25 +0200

    Basic modelling of the app.

---
+++ web_app/evaluator/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=64)
+    code = models.CharField(max_length=3)
+
+
+class Assignment(models.Model):
+    title = models.CharField(max_length=32)
+    body = models.TextField()
+    added = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    deadline = models.DateTimeField()
+
+
+class Submission(models.Model):
+    added = models.DateTimeField(auto_now_add=True)
+    score = models.PositiveSmallIntegerField()
+    metadata = models.CharField(max_length=1024)
+
+
+class Professor(models.Model):
+    user = models.OneToOneField(User)
+
+
+class Student(models.Model):
+    user = models.OneToOneField(User)
