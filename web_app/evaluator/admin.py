ff09fd344bafdb110d64383ff1c7e87cfa92816c
Mihai Iachimovschi <mihai.iachimovschi@gmail.com>
2015-04-22 20:15:29 +0300

    Registering the models in order to be able to add entities from the admin interface.

--- web_app/evaluator/admin.py
+++ web_app/evaluator/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Assignment
+from .models import Course
+from .models import Professor
+from .models import Student
+from .models import StudentGroup
 
-# Register your models here.
+
+admin.site.register(Professor)
+admin.site.register(Student)
+
+admin.site.register(StudentGroup)
+admin.site.register(Course)
+admin.site.register(Assignment)
