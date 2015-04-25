1e219da55130fc3c229e9cdf34134ab2a8517a8d
Mihai Iachimovschi <mihai.iachimovschi@gmail.com>
2015-04-25 20:38:46 +0300

    Moving the list of courses to /courses.

--- web_app/evaluator/urls.py
+++ web_app/evaluator/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = patterns(
     '',
-    url(r'^$', views.Dashboard.as_view(), name="index"),
+    url(r'^$', views.Dashboard.as_view(), name="dashboard"),
+    url(r'^courses$', views.Courses.as_view(), name="courses_list"),
     # url(r'^feed$', None, name=None),
-    # url(r'^courses$', None, name=None),
     # url(r'^courses/NUMBER$', None, name=None),
     # url(r'^assignments/NUMBER$', None, name=None),
 
--- web_app/evaluator/views.py
+++ web_app/evaluator/views.py
@@ -1,8 +1,21 @@
 from django.views import generic
-from .models import Course
+from .models import Course, Professor, Student, StudentGroup
 
 
-class Dashboard(generic.ListView):
-    queryset = Course.objects.all()
+class Dashboard(generic.TemplateView):
     template_name = "dashboard.html"
+
+
+class Courses(generic.ListView):
+    queryset = Course.objects.all()
+    template_name = "courses.html"
     paginate_by = 5
+
+    def get_queryset(self):
+        student = Student.objects.filter(user=self.request.user)
+        professor = Professor.objects.filter(user=self.request.user)
+        if student:
+            c_student_group = StudentGroup.objects.filter(student=student)
+            return Course.objects.filter(studentgroup=c_student_group)
+        elif professor:
+            return Course.objects.filter(professor=professor)
