a6eafdca9aef36faa73bd128d7c779109136b2cb
Mihai Iachimovschi <mihai.iachimovschi@gmail.com>
2015-04-25 18:15:25 +0300

    Tuning the authentication feature.

--- web_app/openlab/urls.py
+++ web_app/openlab/urls.py
@@ -6,6 +6,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'openlab.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^login/$', 'django.contrib.auth.views.login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('evaluator.urls')),
 )
