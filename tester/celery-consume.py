89429a3e66e5dc0326aa2cd38d86c6726282bcea
Mihai Iachimovschi <mihai.iachimovschi@gmail.com>
2015-05-08 21:48:31 +0300

    Tune the test component. Now the results of the tests are written to the database.

--- tester/celery-consume.py
+++ tester/celery-consume.py
@@ -2,9 +2,11 @@ from celery import Celery
 from celery import bootsteps
 from kombu import Consumer, Exchange, Queue
 from docker import Docker
+import psycopg2
+import json
 
 
-class MyConsumerStep(bootsteps.ConsumerStep):
+class CustomConsumerStep(bootsteps.ConsumerStep):
     queue = Queue('default', Exchange('default'), 'default')
 
     def get_consumers(self, channel):
@@ -15,14 +17,66 @@ class MyConsumerStep(bootsteps.ConsumerStep):
 
     def handle_message(self, body, message):
         print('Received message: {0!r}'.format(body))
+        submission_id = body['args'][0]
+        files = get_test_and_solution(submission_id)
+
         docker = Docker()
-        test = open('/app/python/evaluator_test.py')
-        solution = open('/app/python/solution.py')
+        test = open('/app/evaluator_tests/' + files[0])
+        solution = open('/app/evaluator_submissions/' + files[1])
         out = docker.run('python', test, solution)
-        print(out)
-        message.ack()
+        metadata = json.loads(out)
+        try:
+            if metadata['score']:
+                status = 'P'
+            else:
+                status = 'F'
+
+            score = metadata['score']
+        except KeyError:
+            status = 'F'
+            score = 0
+
+        if update_submission(submission_id, score, out, status):
+            message.ack()
 
 
 # TODO: Use env variables for RabbitMQ credentials
 app = Celery(broker='amqp://guest:guest@rabbit//')
-app.steps['consumer'].add(MyConsumerStep)
+app.steps['consumer'].add(CustomConsumerStep)
+
+
+def get_conn():
+    # TODO: Use env variables for PostgreSQL credentials
+    conn = psycopg2.connect(database="postgres", user="postgres", host="db")
+    return conn
+
+
+def get_test_and_solution(submission_id):
+    conn = get_conn()
+    # Open a cursor to perform database operations
+    cur = conn.cursor()
+    cur.execute("SELECT assignment_id, file \
+                FROM evaluator_submission WHERE id=%s", (submission_id,))
+    submission = cur.fetchone()
+
+    cur.execute("SELECT file \
+                FROM evaluator_testcase \
+                WHERE assignment_id=%s AND lang='P'", (submission[0],))
+    testcase = cur.fetchone()
+
+    cur.close()
+    conn.close()
+    return testcase[0], submission[1]
+
+
+def update_submission(id, score, metadata, status):
+    conn = get_conn()
+    cur = conn.cursor()
+    cur.execute("UPDATE evaluator_submission \
+                SET score=%s, metadata=%s, status=%s \
+                WHERE id=%s", (score, metadata, status, id))
+    conn.commit()
+    status = cur.statusmessage
+    cur.close()
+    conn.close()
+    return True if status else False
