447881616eba01cd0dad0fb9535afd8cc354b88f
Mihai Iachimovschi <mihai.iachimovschi@gmail.com>
2015-06-07 21:09:15 +0300

    Improving the post-receive hook.

--- deployment_scripts/git-post-receive-hook.sh
+++ deployment_scripts/git-post-receive-hook.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-PROJECT_ROOT="$HOME/openlab"
+PROJECT_ROOT="$HOME/openlab_deploy"
+COMPOSE_PARAMS="-p openlab -f docker-compose-dev.yml"
 
 while read oldrev newrev refname
 do
     branch=$(git rev-parse --symbolic --abbrev-ref $refname)
-    if [ "master" == "$branch" ]; then
-        GIT_WORK_TREE="$PROJECT_ROOT" git checkout -f master
+    if [ "develop" == "$branch" ]; then
+        GIT_WORK_TREE="$PROJECT_ROOT" git checkout -f develop
         cd "$PROJECT_ROOT/deployment_scripts"
-        docker-compose -p openlab -f docker-compose.yml kill web
-        docker-compose -p openlab -f docker-compose.yml build --no-cache web
-        docker-compose -p openlab -f docker-compose.yml up -d --no-recreate
+        docker-compose $COMPOSE_PARAMS kill web
+        docker-compose $COMPOSE_PARAMS build --no-cache web
+        docker-compose $COMPOSE_PARAMS up -d --no-recreate
+        docker-compose $COMPOSE_PARAMS exec web python manage.py shell
     fi
 done
